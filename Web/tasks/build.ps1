d81878c0b9d21872b3022b3fe940be897ba1484c
Don <drolling@intrepiddirect.com>
2019-05-30 23:03:33 -0500

    making the build so that it runs from more than one location.

--- Web/tasks/build.ps1
+++ Web/tasks/build.ps1
@@ -1,2 +1,9 @@
+$path = Get-Location
+Write-Output $path 
+if(!$path.ToString().Contains('Web')){
+    Set-Location -Path "$path\Web"
+}
+$location = Get-Location
+Write-Output $location 
 tsc
 browserify js/app/main.js > js/app/bundle.js
